fa0c4709fad5fabab593aeead9f735fdb9487c44
Stratulat Sofia <coone4ka@gmail.com>
2026-09-19 21:46:25 +0300

    Add files via upload

---
+++ tema 15.09.py
@@ -0,0 +1,30 @@
+l = 8
+L = 13
+A = l * L # Area of rectangle
+P = 2 * (l + L) # Perimeter of rectangle
+
+print ("Area of rectangle is: ", A)
+print ("Perimeter of rectangle is: ", P)
+
+minutes = 95
+hours = minutes // 60
+remaining_minutes = minutes % 60
+
+print ("Total time is: ", hours, "hours and", remaining_minutes, "minutes")
+
+n = number = 5
+
+print ( n % 2 == 0)
+
+print ( 2 * 3 * (2 + 9))
+print ( 2 * 3 * 2 + 9)
+print ( (5 - 2) ** 2 + 3)
+print ( 5 - 2 ** 2 + 3)
+print ( (10 / 2) ** 3 + 2)
+print ( 10 / 2 ** 3 + 2)
+
+x = 10
+
+print ( x > 5 and x < 15)
+print ( x < 5 or x >= 11)
+print ( not ( x > 5))
